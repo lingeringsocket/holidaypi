62df50fbacbb0eddfa2c3392214141f18551c0b2
John Sichi <jsichi@gmail.com>
2016-09-13 15:53:53 -0700

    Add vintage filter in comments.

--- develop.sh
+++ develop.sh
@@ -15,4 +15,8 @@ done
 
 MP4Box -fps ${VIDEO_FRAMERATE} ${opts} film.mp4
 
+# for a more old-timey effect, use
+# ffmpeg -i film.mp4 -vf "curves=preset=vintage" vintage.mp4
+# or something like https://github.com/mazurkin/mkfilm_old
+
 popd
