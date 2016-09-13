c5d0a43727a3f147e7b68960a59b8fde4baa1bf4
John Sichi <jsichi@gmail.com>
2016-09-13 15:44:36 -0700

    Oops use rotate, not flip.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Readably by both python and bash
 
 VIDEO_PATH="/home/pi/holidaypi/film/"
-VIDEO_VFLIP=True
+VIDEO_VFLIP=False
 VIDEO_FRAMERATE=16
-VIDEO_ROTATE=0
+VIDEO_ROTATE=180
 VIDEO_RESOLUTION=(1280, 720)
 
 BUTTONGPIO=16
